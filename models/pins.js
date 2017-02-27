b3f6bfd1c447c127ad7e4861e4364fc53bdd7d90
ReeceLangerock <reecelangerock@gmail.com>
2017-02-26 22:28:20 -0600

    updated models for pins and users

--- models/pins.js
+++ models/pins.js
@@ -1,27 +1,31 @@
 const mongoose = require('mongoose');
 var ObjectID = require('mongodb').ObjectID;
-var userModel = require('../models/user');
+var userModel = require('../models/users');
 
 var pinSchema = mongoose.Schema({
     '_id': String,
     'pinOwner': String,
+    'pinOwnerID': String,
     'pinUrl': String,
     'pinTitle': String,
     'pinDescription': String,
+    'creationDate': Date,
     // pinLikes will be array of userID that have liked a image, get total number by .length
-    'pinLikes': [String]
+    'pinnedBy': [String]
 
 });
 
-pinSchema.methods.newPin = function(url, title, description, owner, ownerHREF) {
+pinSchema.methods.newPin = function(url, title, description, owner, ownerID) {
     var tempID = new ObjectID();
     var newPin = new pinModel({
         '_id': tempID,
         'pinOwner': owner,
+        'pinOwnerID': ownerID,
         'pinUrl': url,
         'pinTitle': title,
         'pinDescription': description,
-        'pinLikes': []
+        'creationDate': new Date(),
+        'pinnedBy': [ownerID]
     })
 
     newPin.save(function(err) {
@@ -32,10 +36,10 @@ pinSchema.methods.newPin = function(url, title, description, owner, ownerHREF) {
         }
     }).then(function(){
       userModel.findOneAndUpdate({ // find user doc
-          userHREF: ownerHREF
+          '_id': ownerID
       }, {
           $push: {
-              'pins': tempID //add newly create poll to the user doc
+              'pinnedPins': tempID //add newly create poll to the user doc
           }
       }, {
           safe: true,
