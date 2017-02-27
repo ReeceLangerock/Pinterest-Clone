4815d8534fd9a8fed3535eb6cb4fd4d9d5023db0
ReeceLangerock <reecelangerock@gmail.com>
2017-02-26 22:21:03 -0600

    clean up

--- app.js
+++ app.js
@@ -7,10 +7,7 @@ var port = process.env.PORT || 3000;
 var config = require('./config');
 var stormpath = require('express-stormpath');
 var apiKey = config.getSTORMPATH_CLIENT_APIKEY_ID();
-//var passport = require('passport');
-//var session = require('express-session');
-//var cookieSession = require('cookie-session');
-
+var mongoLoginHandler = require('./controllers/mongoLoginHandler.js')
 
 //MONGOOSE CONFIG
 mongoose.connect('mongodb://'+config.getMongoUser()+':'+config.getMongoPass()+'@ds157459.mlab.com:57459/pinterest-clone-srl');
@@ -21,7 +18,6 @@ db.on('error', console.error.bind(console, 'connection eror:'));
 db.once('open', function(){
   console.log("connected");
 })
-
 
 //EXPRESS SETUP
 app.set('views', path.join(__dirname, '/views'));
@@ -34,34 +30,21 @@ app.use(function(err, req, res, next){
   return;
 })
 
-/*
-app.use(cookieSession({
-  name: 'session',
-  keys: [process.env.cookieSecret],
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}));
-
-//passport setup
-app.use(session(process.env.passportSecret));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(function(req, res, next) {
-  res.locals.currentUser = req.user;
-  next();
-});
-
-app.use('/', require('./controllers/index2'));
-app.use('/auth', require('./controllers/signin'));
-app.use('/logout', require('./controllers/logout'))
-*/
-
+//STORMPATH SETUP
 app.use(stormpath.init(app, {
+  postLoginHandler: function (account, req, res, next) {
+    // check to see if mongo doc has been made for user
+    mongoLoginHandler.handleLogin(account);
+    next();
+  },
   website: true
 }));
 app.set('stormpathRedirectUrl', '/');
 
+//ROUTES
 app.use('/', require('./controllers/index')());
 app.use('/add-pin', require('./controllers/add-pin')());
+app.use('/user', require('./controllers/user')());
 app.use(function (req, res, next) {
   res.status(404).render('404');
 })
