9bb1689ceb3a94dac618b3dc9a0f155e1df43b73
ReeceLangerock <reecelangerock@gmail.com>
2017-02-26 22:26:13 -0600

    add all functionality to create a pin

--- controllers/add-pin.js
+++ controllers/add-pin.js
@@ -2,23 +2,60 @@ var express = require('express');
 var router = express.Router();
 
 var returnRouter = function() {
-    var request = require('request');
-    var stormpath = require('express-stormpath');
-    var bodyParser = require('body-parser');
-    var pins = require('../models/pins.js')
-    router.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    router.use(bodyParser.json());
+        var request = require('request');
+        var stormpath = require('express-stormpath');
+        var bodyParser = require('body-parser');
+        var pins = require('../models/pins.js')
+        var users = require('../models/users.js');
+        router.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        router.use(bodyParser.json());
 
-    router.post('/', stormpath.getUser, function(req, res){      
-      var response = pins.schema.methods.newPin(req.body.image_url, req.body.image_title, req.body.image_description, req.user.givenName, req.user.href);
-      console.log(response);
-      res.redirect('/');
-      res.end();
-    })
+        router.post('/', stormpath.getUser, function(req, res) {
+                var url = req.body.image_url;
+                var title = req.body.image_title;
+                var description = req.body.image_description;
+                var userName = req.user.givenName
+                if (req.user) {
+                    checkForExistingUser(req.user.href).then(function(response, error) {
+                            var userID = response['_id'];
+                            pins.schema.methods.newPin(url, title, description, userName, userID);
+                        })
+                        .then((response, error) => {
+                            res.redirect('back');
+                            res.end();
+                        }).catch(function(err) {
+                            console.log(err);
+                        });
+
+                } else{
+                  console.log('elseing');
+                  res.redirect('back');
+                  res.end();
+                }
+
+              });
 
 
-    return router;
-}
-module.exports = returnRouter;
+            function checkForExistingUser(userHREF) {
+                return new Promise(function(resolve, replace) {
+                    users.findOne({
+                            'userHREF': userHREF
+                        },
+                        function(err, docs) {
+                            if (err) {
+                                console.log('error');
+                                reject(err);
+                            } else if (docs) {
+                                resolve(docs)
+                            } else {
+                                resolve('NOT_FOUND');
+                            }
+                        })
+                });
+            }
+
+            return router;
+        }
+        module.exports = returnRouter;
