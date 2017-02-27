425ae7ce9126c8af84469c033159ba0941e9f122
ReeceLangerock <reecelangerock@gmail.com>
2017-02-26 22:30:08 -0600

    create user model

---
+++ models/users.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+var ObjectID = require('mongodb').ObjectID;
+
+var userSchema = mongoose.Schema({
+    '_id': String,
+    'userHREF': String,
+    'userName': String,
+    'pinnedPins': [String]
+
+
+});
+
+userSchema.methods.newUser = function(href, name){
+
+  var newUser = new userModel({
+    '_id':  new ObjectID(),
+    'userHREF': href,
+    'userName': name,
+    'pinnedPins': []
+  });
+
+  newUser.save(function(err){
+    if(err){
+      throw err;
+    }
+    else{
+      return 'success';
+    }
+  })
+}
+
+var userModel = mongoose.model('user', userSchema, 'users');
+module.exports = userModel;
